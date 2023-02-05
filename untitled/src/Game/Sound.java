532a508d3e1f0afd730a357579bba3742a64aedd
at20984 <at20984@essex.ac.uk>
2023-02-05 03:45:57 +0000

    Changed Ambient track for licence reasons

--- untitled/src/Game/Sound.java
+++ untitled/src/Game/Sound.java
@@ -8,14 +8,14 @@ import java.io.IOException;
 
 public class Sound {
     public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
-        File file = new File("Sound/Minimalism.wav");
+        File file = new File("Sound/Minimalism.wav");//https://soundcloud.com/flat-cap-tiger/minimalism-take-2-re-mastered
         AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
         Clip clip = AudioSystem.getClip();
         clip.open(audioStream);
         FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
         gainControl.setValue(-15.0f); // reduce volume by 15db
         clip.start();
-        File file2 = new File("Sound/birdambient.wav");
+        File file2 = new File("Sound/birdambient.wav"); //https://freesound.org/people/bajko/sounds/385279/
         AudioInputStream audioStream2 = AudioSystem.getAudioInputStream(file2);
         Clip clip2 = AudioSystem.getClip();
         clip2.open(audioStream2);
