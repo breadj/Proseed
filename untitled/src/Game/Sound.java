b5f1cdfa3ea7aa0c6ed292ea111e0819386ba9d8
at20984 <at20984@essex.ac.uk>
2023-02-05 02:57:28 +0000

    Added Ambient

--- untitled/src/Game/Sound.java
+++ untitled/src/Game/Sound.java
@@ -14,8 +14,14 @@ public class Sound {
         clip.open(audioStream);
         FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
         gainControl.setValue(-15.0f); // reduce volume by 15db
-
         clip.start();
+        File file2 = new File("Sound/birdambient.wav");
+        AudioInputStream audioStream2 = AudioSystem.getAudioInputStream(file2);
+        Clip clip2 = AudioSystem.getClip();
+        clip2.open(audioStream2);
+        FloatControl gainControl2 = (FloatControl) clip2.getControl(FloatControl.Type.MASTER_GAIN);
+        gainControl2.setValue(-15.0f); // reduce volume by 15db
+        clip2.start();
     }
 
 
